35f99cc21b2262b6c49e96ec8fc74e261552b005
axel_bass@mail.ru <axel_bass@mail.ru>
2017-07-30 21:33:09 +0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+﻿$(document).delegate("button[data-toggle='alert-success-simple']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("", "Простое оповещение").success.alert();
+});
+$(document).delegate("button[data-toggle='alert-success-header']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("Заголовок", "Текст под заголовком").success.alert();
+});
+$(document).delegate("button[data-toggle='alert-success-full']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("Заголовок", "Текст под заголовком", "Текст с подробностями").success.alert();
+});
+
+$(document).delegate("button[data-toggle='alert-warning-simple']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("", "Простое оповещение").warning.alert();
+});
+$(document).delegate("button[data-toggle='alert-warning-header']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("Заголовок", "Текст под заголовком").warning.alert();
+});
+$(document).delegate("button[data-toggle='alert-warning-full']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("Заголовок", "Текст под заголовком", "Текст с подробностями").warning.alert();
+});
+
+$(document).delegate("button[data-toggle='alert-danger-simple']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("", "Простое оповещение").danger.alert();
+});
+$(document).delegate("button[data-toggle='alert-danger-header']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("Заголовок", "Текст под заголовком").danger.alert();
+});
+$(document).delegate("button[data-toggle='alert-danger-full']", "click", function (e) {
+    e.preventDefault();
+
+    modalConfirm("Заголовок", "Текст под заголовком", "Текст с подробностями").danger.alert();
+});
+
